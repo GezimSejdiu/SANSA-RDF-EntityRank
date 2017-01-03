01b28660aff5f7674a5800c64730dd90a851e7d3
Gezim <g.sejdiu@gmail.com>
2017-01-03 13:21:36 +0100

    Added DependenciesAnnotation

--- sansa-rdf-entityrank-spark/src/main/scala/net/sansa_stack/entityrank/spark/nlp/StanfordAnnotation.scala
+++ sansa-rdf-entityrank-spark/src/main/scala/net/sansa_stack/entityrank/spark/nlp/StanfordAnnotation.scala
@@ -5,26 +5,44 @@ import java.util.Properties
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation
 import edu.stanford.nlp.pipeline.{ Annotation, StanfordCoreNLP }
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentClass
+import edu.stanford.nlp.util.CoreMap
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation
+import edu.stanford.nlp.ling.CoreAnnotations
 
-object StanfordSentimentizer {
+object StanfordAnnotation {
 
   val properties = new Properties()
-  properties.setProperty("annotators", "tokenize, ssplit, pos, parse, sentiment")
+  //properties.setProperty("annotators", "tokenize, ssplit, pos, parse, sentiment")
+  properties.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, sentiment");
   properties.setProperty("ssplit.isOneSentence", "true")
   val pipeline: StanfordCoreNLP = new StanfordCoreNLP(properties)
 
-  
   def Sentiment(sentence: String): String = {
 
     // create an empty Annotation just with the given text
     val document: Annotation = new Annotation(sentence)
-    
+
     // run all Annotators on this text
     pipeline.annotate(document)
 
     val sentiment: String = document.get(classOf[SentencesAnnotation]).get(0).get(classOf[SentimentClass])
 
     sentiment
+  }
+
+  def Dependencies(sentence: String): String = {
+
+    // create an empty Annotation just with the given text
+    val document: Annotation = new Annotation(sentence)
+
+    // run all Annotators on this text
+    pipeline.annotate(document)
+
+    val sentences: CoreMap = document.get(classOf[SentencesAnnotation]).get(0)
+
+    val dependencies = sentences.get(classOf[CollapsedCCProcessedDependenciesAnnotation])
+
+    dependencies.toString()
   }
 
 }
