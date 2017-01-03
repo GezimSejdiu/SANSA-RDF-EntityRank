acbaf1a4365e9c6771dfae5497045d2be79b43a9
Gezim <g.sejdiu@gmail.com>
2017-01-03 13:22:57 +0100

    Started impl. implicit mapper for EntityRank DF

--- sansa-rdf-entityrank-spark/src/main/scala/net/sansa_stack/entityrank/spark/ranking/EntityRank.scala
+++ sansa-rdf-entityrank-spark/src/main/scala/net/sansa_stack/entityrank/spark/ranking/EntityRank.scala
@@ -2,8 +2,11 @@ package net.sansa_stack.entityrank.spark.ranking
 
 import com.hp.hpl.jena.graph.{ Node => JNode }
 import org.apache.spark.rdd.RDD
-import com.hp.hpl.jena.graph.{Node => JNode}
 import net.sansa_stack.entityrank.spark.model.Triples
+import org.apache.spark.sql.{SparkSession, DataFrame }
+import net.sansa_stack.entityrank.spark.model.TriplesDataFrame._
+import net.sansa_stack.entityrank.spark.nlp.StanfordAnnotation
+import org.apache.spark.sql.functions.udf
 
 class EntityRank extends IRank {
 
@@ -18,4 +21,40 @@ class EntityRank extends IRank {
       case Triples(s, p, o) => (s, 0.0)
     }
   }
+}
+
+/**
+ * Implicit mapper for EntityRank dataframe
+ */
+object EntityRankDataFrame {
+
+  implicit class EntityRankDF(val data: DataFrame) {
+    def rescale(): DataFrame = {
+
+      // Split the data into training and test sets (30% held out for testing)
+      val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3), 1234L) //seed = 1234L)
+
+      val tokenizedTrainingData = tokenize(trainingData, "entities", "tokens")
+      val tokenizedTestData = tokenize(testData, "entities", "tokens")
+
+      val (rescaledCorpus, idfModel) = TFIDF(tokenizedTrainingData, "tokens", 100000, 2)
+      rescaledCorpus
+    }
+
+    def highestRankedResources()(implicit spark: SparkSession): DataFrame = {
+      import spark.implicits._
+      data.groupBy($"entities").avg("rank").sort($"avg(rank)".desc)
+    }
+
+    def generateRanking()(implicit spark: SparkSession): DataFrame = {
+      import spark.implicits._
+      data
+        .sample(withReplacement = true, 0.001, 1234)
+        .withColumn("EntitySentiment", getSentiment($"entity"))
+    }
+  }
+
+  //udf function for sentiment anotation
+  val sentimentArg: String => String = (param: String) => StanfordAnnotation.Sentiment(param)
+  val getSentiment = udf(sentimentArg)
 }
